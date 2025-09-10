19fc947274d675a1b2fece7a81ccb242593ebab1
Florido-hub <floridodinizz@gmail.com>
2025-09-10 20:35:33 -0300

    feat(product): cria endpoints Get e GetById

---
+++ src/main/java/com/github/leopc17/inventorymanager/application/ProductServiceImpl.java
@@ -0,0 +1,47 @@
+package com.github.leopc17.inventorymanager.application;
+
+import com.github.leopc17.inventorymanager.domain.entity.Product;
+import com.github.leopc17.inventorymanager.domain.input.ProductServicePort;
+import com.github.leopc17.inventorymanager.infrastructure.entity.adapter.output.ProductReporitoryAdapter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductServiceImpl implements ProductServicePort {
+
+    private final ProductReporitoryAdapter productReporitoryAdapter;
+
+    @Autowired
+    public ProductServiceImpl(ProductReporitoryAdapter productReporitoryAdapter) {
+        this.productReporitoryAdapter = productReporitoryAdapter;
+    }
+
+    @Override
+    public Product create(Product product) {
+        return productReporitoryAdapter.create(product).get();
+    }
+
+    @Override
+    public List<Product> getAllProducts() {
+        var optional = productReporitoryAdapter.getAllProducts();
+        List<Product> list = optional.get();
+        return list;
+    }
+
+    @Override
+    public Product getProductById(Integer id) {
+        return productReporitoryAdapter.getProductById(id).get();
+    }
+
+    @Override
+    public Product updateProduct(Product product, Integer id) {
+        return null;
+    }
+
+    @Override
+    public void deleteProductById(Integer id) {
+
+    }
+}
--- src/main/java/com/github/leopc17/inventorymanager/infrastructure/entity/adapter/input/ProductController.java
+++ src/main/java/com/github/leopc17/inventorymanager/infrastructure/entity/adapter/input/ProductController.java
@@ -1,4 +1,9 @@
 package com.github.leopc17.inventorymanager.infrastructure.entity.adapter.input;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/products")
 public class ProductController {
 }
