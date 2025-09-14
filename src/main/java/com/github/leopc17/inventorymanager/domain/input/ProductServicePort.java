3aa0d14ba383f3691157ceab33525e9011e79c1c
Matheusgs083 <matheus.gs083@gmail.com>
2025-09-14 15:00:51 -0300

    feat(product): adicionar endpoint PATCH para atualizar quantidade em estoque

--- src/main/java/com/github/leopc17/inventorymanager/domain/input/ProductServicePort.java
+++ src/main/java/com/github/leopc17/inventorymanager/domain/input/ProductServicePort.java
@@ -1,8 +1,8 @@
 package com.github.leopc17.inventorymanager.domain.input;
 
-import com.github.leopc17.inventorymanager.domain.model.Product;
-
 import java.util.List;
+
+import com.github.leopc17.inventorymanager.domain.model.Product;
 
 public interface ProductServicePort {
     Product create(Product product);
@@ -11,4 +11,5 @@ public interface ProductServicePort {
     Product update(Product newProduct, Integer id);
     void deleteById(Integer id);
     Product updateInventory(Integer id, Integer quantity);
+    
 }
